ad3ec1a3cd8a817728b1f0d104f3f225483614c0
Tom Irimescu <tom@irimescu.com>
2022-02-27 17:50:46 -0500

    Update cartActions.js

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const { data } = await axios.get(`https://mern-e-commerce-api-sxv2z.ondigitalocean.app/api/products/${id}`)
+  const { data } = await axios.get(`/api/products/${id}`)
 
   dispatch({
     type: CART_ADD_ITEM,
