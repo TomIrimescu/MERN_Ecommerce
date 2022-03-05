033ae87982bcd4b85e2156c592502d0e626bc052
Tom Irimescu <tom@irimescu.com>
2022-03-05 13:59:01 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,14 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
+app.use((req, res, next) => {
+  res,setHeader('Access-Control-Allow-Origin', 'https://mern-e-commerce-api-sxv2z.ondigitalocean.app')
+  res,setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res,setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type')
+  res,setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
+
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 
